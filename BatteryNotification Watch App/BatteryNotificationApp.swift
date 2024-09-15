e8d2e6110198f262131e35ff752ecbd0c429db12
Jun Nakarai <m3h138@icloud.com>
2024-09-16 07:53:51 +0900

    Initial Commit

---
+++ BatteryNotification Watch App/BatteryNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryNotificationApp.swift
+//  BatteryNotification Watch App
+//
+//  Created by Jun Nakarai on 2024/09/16.
+//
+
+import SwiftUI
+
+@main
+struct BatteryNotification_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryNotification Watch AppTests/BatteryNotification_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryNotification_Watch_AppTests.swift
+//  BatteryNotification Watch AppTests
+//
+//  Created by Jun Nakarai on 2024/09/16.
+//
+
+import Testing
+@testable import BatteryNotification_Watch_App
+
+struct BatteryNotification_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
